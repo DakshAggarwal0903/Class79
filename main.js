c5de41581d3ce9a29ea096452ba0a0400abd0fe3
StormMaster99 <74252852+StormMaster99@users.noreply.github.com>
2021-04-13 15:37:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+ var abc = [];
+function submit(){
+var a1 = document.getElementById("S1").value;
+var a2 = document.getElementById("S2").value;
+var a3 = document.getElementById("S3").value;
+var a4 = document.getElementById("S4").value;
+abc.push[a1];
+abc.push[a2];
+abc.push[a3];
+abc.push[a4];
+console.log(abc);
+document.getElementById("answer").innerHTML = abc;
+}
